7f32db71f3f49913708d12942163da6615917205
moeabm <moeabm@yahoo.com>
2014-01-30 10:33:44 -0800

    added came launcher

---
+++ LaunchGame.bat
@@ -0,0 +1,5 @@
+Set GAMEPATH="F:\SteamLibrary\SteamApps\common\Source SDK Base 2013 Multiplayer\hl2.exe"
+SET MODPATH="F:\Dev\HL_Modding\VS2013\template_mp\game\mod_template_mp"
+
+
+%GAMEPATH% -allowdebug -novid  -insecure -sw -game %MODPATH%
