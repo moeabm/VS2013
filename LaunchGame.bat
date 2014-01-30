38c92b97016f79125054d11328ab4def85f0c7d5
moeabm <moeabm@yahoo.com>
2014-01-30 11:34:07 -0800

    Added GameLauncher

--- LaunchGame.bat
+++ LaunchGame.bat
@@ -2,4 +2,4 @@ Set GAMEPATH="F:\SteamLibrary\SteamApps\common\Source SDK Base 2013 Multiplayer\
 SET MODPATH="F:\Dev\HL_Modding\VS2013\template_mp\game\mod_template_mp"
 
 
-%GAMEPATH% -allowdebug -novid  -insecure -sw -game %MODPATH%
+%GAMEPATH% -allowdebug -novid  -insecure -sw -game %MODPATH% +sv_lan 0
